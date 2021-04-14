6964d61dfa59675fc222c82e0b7d665dbbcf6099
leRisen <lemmas.online@gmail.com>
2021-04-14 20:33:44 +0300

    Add laravel ide helper

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Product
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $price
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\ProductAttribute[] $attributes
+ * @property-read int|null $attributes_count
+ * @method static \Illuminate\Database\Eloquent\Builder|Product newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Product newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Product query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Product extends Model
 {
     use HasFactory;
--- app/Models/ProductAttribute.php
+++ app/Models/ProductAttribute.php
@@ -5,6 +5,27 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\ProductAttribute
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $value
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property int $product_id
+ * @property-read \App\Models\Product $product
+ * @method static \Illuminate\Database\Eloquent\Builder|ProductAttribute newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|ProductAttribute newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|ProductAttribute query()
+ * @method static \Illuminate\Database\Eloquent\Builder|ProductAttribute whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ProductAttribute whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ProductAttribute whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ProductAttribute whereProductId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ProductAttribute whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ProductAttribute whereValue($value)
+ * @mixin \Eloquent
+ */
 class ProductAttribute extends Model
 {
     use HasFactory;
